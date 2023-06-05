20ce7faf8fef0c14fedd798ff047ed2de9f598e9
SlawekKaczmarczyk <slawomir.kaczmarczyk1998@gmail.com>
2023-06-05 09:37:20 +0200

    First funcionality

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
 
 PATH ="C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 
+
+
 driver.get("https://github.com")
 print(driver.title)
+
+search = driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/header/div/div[2]/div/div/div[1]/div/div/form/label/input[1]")
+search.send_keys("test")
+search.send_keys(Keys.RETURN)
+
 driver.close()
