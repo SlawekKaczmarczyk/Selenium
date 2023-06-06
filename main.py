e8fce0d9b2904b06b66dffcac11642b2802bc488
SlawekKaczmarczyk <slawomir.kaczmarczyk1998@gmail.com>
2023-06-06 19:55:11 +0200

    Fixing not seeing search field

--- main.py
+++ main.py
@@ -10,9 +10,10 @@ driver = webdriver.Chrome(PATH)
 
 driver.get("https://github.com")
 print(driver.title)
+driver.maximize_window() # without this, it cannot see search field
 
 search = driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/header/div/div[2]/div/div/div[1]/div/div/form/label/input[1]")
 search.send_keys("test")
 search.send_keys(Keys.RETURN)
-
+time.sleep(5)
 driver.close()
