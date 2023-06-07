a144b4a659b4ac35a80bae4ff35115f28c735c7b
SlawekKaczmarczyk <slawomir.kaczmarczyk1998@gmail.com>
2023-06-07 11:29:41 +0200

    More features

--- clicking_test.py
+++ clicking_test.py
@@ -9,11 +9,23 @@ PATH ="C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 
 
-
 driver.get("https://github.com")
 print(driver.title)
 driver.maximize_window() # without this, it cannot see search field
 
 link = driver.find_element(By.LINK_TEXT, 'Pricing').click()
-time.sleep(5)
-driver.close()
+
+try:
+    element = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.LINK_TEXT, 'Features')))
+    element.click()
+    time.sleep(0.5) # added for visual purposes
+    driver.back()
+    time.sleep(0.5)
+    driver.forward()
+    time.sleep(0.5)
+    element.clear()
+
+except:
+    time.sleep(1)
+    driver.close()
