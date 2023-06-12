7e2a407fd5c7a8e7a1f1f6a969e1bd1f0aaef53a
SlawekKaczmarczyk <slawomir.kaczmarczyk1998@gmail.com>
2023-06-12 06:45:51 +0200

    Pass by cookies consent

--- action_chains.py
+++ action_chains.py
@@ -1,10 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+import time
 
 PATH ="C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 driver.get("https://orteil.dashnet.org/cookieclicker/")
+
+consent = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "/html/body/div[3]/div[2]/div[1]/div[2]/div[2]/button[1]/p"))).click()
+
 
 driver.implicitly_wait(5)
 
@@ -17,3 +24,5 @@ actions.click(cookie)
 
 for i in range(5000):
     actions.perform()
+
+    
