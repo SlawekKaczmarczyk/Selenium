1b4b579c82f5cf56c12a7dc4585bf11dd8df539e
SlawekKaczmarczyk <slawomir.kaczmarczyk1998@gmail.com>
2023-06-20 21:29:51 +0200

    Title test

--- test case/main.py
+++ test case/main.py
@@ -8,12 +8,9 @@ class PythonOrgSearch(unittest.TestCase):
         self.driver = webdriver.Chrome("C:\Program Files (x86)\chromedriver.exe")
         self.driver.get("http://python.org")
 
-    def test_example(self):
-        print("Test")
-        assert False 
-
-    def test_example_2(self):
-        assert True 
+    def test_title(self):
+        mainPage = page.MainPage()
+        assert mainPage.is_title_matches()
 
     def tearDown(self):
         self.driver.close()
